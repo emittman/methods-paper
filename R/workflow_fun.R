340e4efc56f0c7381629a8d38f27e632f26bf6fc
emittman <emittman@iastate.edu>
2017-09-04 10:36:51 -0500

    Run a very long chain (heterosis)

--- R/workflow_fun.R
+++ R/workflow_fun.R
@@ -11,7 +11,7 @@ initialize_chain <- function(seed, methodPi){
   
   contr <- formatControl(n_iter = 2,
                          thin = 1,
-                         warmup = 1000,
+                         warmup = 10000,
                          methodPi = "symmDirichlet",
                          idx_save = 1,
                          n_save_P = 1,
@@ -30,10 +30,10 @@ initialize_chain <- function(seed, methodPi){
                                  alpha = alpha,
                                  C = C))
   
-  contr$n_iter <- as.integer(50000)
+  contr$n_iter <- as.integer(1000000)
   contr$thin <- as.integer(5)
   contr$warmup <- as.integer(10000)
-  contr$idx_save <- 0:9
+  contr$idx_save <- 1
   contr$n_save_P <- as.integer(100)
   contr$methodPi <- methodPi
   
