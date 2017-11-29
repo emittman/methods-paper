2c8ceac88d6558da8604bdb44682ae240c9fdf3c
emittman <emittman@iastate.edu>
2017-11-28 18:04:38 -0600

    Edits to abstract
    -Add computation of diagnostics for simulation study 1

--- R/sim-study-1/geweke.R
+++ R/sim-study-1/geweke.R
@@ -1,17 +1,43 @@
 #geweke
 
-#sim1
-mcmc1 <- readRDS("mcmc_sim_1")
-str(mcmc1$samples$beta)
+# #sim1
+# mcmc1 <- readRDS("mcmc_sim_1")
+# str(mcmc1$samples$beta)
 
 library(plyr)
 library(coda)
 
-geweke <- adply(mcmc1$samples$beta, c(1, 3), function(ch){
-  data.frame(geweke = geweke.diag(ch)[[1]])
-})
+diagnostics <- function(sims){
+  ldply(sims, function(i){
+    mcmc <- readRDS(paste("mcmc_sim_",i,sep=""))
+    geweke <- adply(mcmc$samples$beta, c(1, 3), function(ch){
+      data.frame(geweke = geweke.diag(ch)[[1]],
+                 n_eff = effectiveSize(ch))
+    })
+    geweke
+  })
+}
+
+diag13 <- diagnostics(1:3)
+diag13$sim <- rep(1:3, each=50000)
+save(diag13, file="convergence1_3.RData")
+
+diag46 <- diagnostics(4:6)
+diag46$sim <- rep(4:6, each=50000)
+save(diag46, file="convergence4_6.RData")
+
+diag70 <- diagnostics(7:10)
+diag70$sim <- rep(7:10, each=50000)
+save(diag70, file="convergence7_10.RData")
 
 library(ggplot2)
 
-ggplot(geweke, aes(x=geweke)) + geom_density(fill="blue", alpha=.5) +
-  facet_wrap(~v) + stat_function(fun = dnorm)
+ggplot(diag13, aes(sample=geweke)) + geom_qq(fill="blue", alpha=.5, pch=".") +
+  facet_grid(sim~v) + geom_abline(slope=1)#stat_function(fun = dnorm)
+
+library(dplyr)
+mutate(diag13, v=factor(v, levels=1:5, labels=c("intercept", "parental HD", "hybrid", "hybrid HD", "flow cell"))) %>%
+ggplot(aes(x=n_eff)) + geom_histogram() +
+  facet_grid(sim~v) + scale_x_continuous(breaks=c(0,2000,4000,6000), limits=c(0,6000))
+
+ddply(diag13, .(v,sim), summarise, min_neff = min(n_eff))
